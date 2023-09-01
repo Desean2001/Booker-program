a1da5f2de2f57ec98bd312b4b0905ad9338af3ce
Sean Finley <finleysean9@gmail.com>
2023-08-31 20:54:23 -0400

    created function that loops numbers and sorts them into the before current hour or not and assigns new classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+  
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  test2();
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -23,3 +26,40 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
   $('#currentDay').text(dayjs().format('MMM D, YYYY'));
 })
+
+function test2 () {
+  for (let i=9; i <= 17; i++) {
+    test3(i);
+  }
+}
+
+function test3 (hour) {
+  let currentHour = dayjs().format('HH');
+  if (hour < currentHour) {
+    let classHour = $(`#hour-${hour}`);
+    if (classHour.hasClass("past")) {
+    } else {
+      classHour.removeClass("present");
+      classHour.removeClass("future");
+      classHour.addClass("past");
+    }
+  } else if (hour > currentHour) {
+    let classHour = $(`#hour-${hour}`);
+    if (classHour.hasClass("future")) {
+    } else {
+      let classHour = $(`#hour-${hour}`);
+      classHour.removeClass("present");
+      classHour.removeClass("past");
+      classHour.addClass("future");
+    }
+  } else {
+    let classHour = $(`#hour-${hour}`);
+    if (classHour.hasClass("present")) {
+    } else {
+      let classHour = $(`#hour-${hour}`);
+      classHour.removeClass("past");
+      classHour.removeClass("future");
+      classHour.addClass("present");
+    }
+  }
+}
